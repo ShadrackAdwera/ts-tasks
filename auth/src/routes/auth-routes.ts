fc23bf84c9c9bb9cc3acbd9b07bb04c876a2d7f2
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-11 07:05:48 +0300

    create auth routers

---
+++ auth/src/routes/auth-routes.ts
@@ -0,0 +1,29 @@
+import { body } from 'express-validator';
+import express from 'express';
+import mongoose from 'mongoose';
+
+import { signUp, login, requestPasswordReset, resetPassword } from '../controllers/auth-controllers';
+
+const router = express.Router();
+
+router.post('/sign-up', [
+    body('username').trim().not().isEmpty(),
+    body('email').normalizeEmail().isEmail(),
+    body('password').trim().isLength({ min: 6 }),
+    body('company').not().isEmpty().custom(input=>mongoose.Types.ObjectId.isValid(input))
+], signUp);
+
+router.post('/login', [
+    body('email').normalizeEmail().isEmail(),
+    body('password').trim().isLength({ min: 6 })
+], login);
+
+router.patch('/request-reset-token', [
+    body('email').normalizeEmail().isEmail()
+], requestPasswordReset);
+
+router.patch('/reset-password/:resetToken', [
+    body('password').trim().isLength({ min: 6 })
+], resetPassword);
+
+export { router as authRouter };
