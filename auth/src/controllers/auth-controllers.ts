4bfee7ebf95bb0f048ce0af9082e00451b379380
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-11 06:28:10 +0300

    create login controller

--- auth/src/controllers/auth-controllers.ts
+++ auth/src/controllers/auth-controllers.ts
@@ -12,7 +12,9 @@ interface UserAttributes extends Document {
     username: string;
     email: string;
     password: string;
-    company: string;
+    company: {
+        name: string;
+    } | string;
     roles?: string[];
 }
 
@@ -38,7 +40,7 @@ const signUp = async(req: Request, res: Response, next: NextFunction) => {
 
     //check if email exists in the DB
     try {
-        foundUser = await User.findOne({email}).exec();
+        foundUser = await User.findOne({email}).populate('company').exec();
     } catch (error) {
         return next(new HttpError('An error occured, try again', 500));
     }
@@ -101,4 +103,48 @@ const signUp = async(req: Request, res: Response, next: NextFunction) => {
     res.status(201).json({message: 'Sign Up successful', user: { id: newUser.id, email, company: newCompany.name ,token }});
 
 }
+
+const login = async(req: Request, res: Response, next: NextFunction) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()) {
+        return next(new HttpError('Invalid inputs', 422));
+    }
+    let foundUser: UserAttributes;
+    let isPassword: boolean;
+    let token: string;
+    const { email, password } = req.body;
+
+    //check if email exists in the DB
+    try {
+        foundUser = await User.findOne({email}).populate('company').exec();
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+    if(!foundUser) {
+        return next(new HttpError('Email does not exist, sign up instead', 400));
+    }
+
+    //compare passwords
+    try {
+        isPassword = await bcrypt.compare(password, foundUser.password);
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+
+    if(!isPassword) {
+        return next(new HttpError('Invalid password', 422));
+    }
+
+    //generate token
+    try {
+        token = await jwt.sign( { id: foundUser.id, email: foundUser.email }, process.env.JWT_KEY, { expiresIn: '1h' });    
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+
+    //TODO: Add company name to response body.
+    res.status(201).json({message: 'Login Successful', user: { id: foundUser.id, email, token, roles: foundUser.roles }})
+}
+
+
 
