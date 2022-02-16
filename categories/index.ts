0af9761b9922ca6d24febbda302dd878616951d3
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-16 21:41:37 +0300

    set up route handlers for categories

---
+++ categories/index.ts
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+
+
+if(!process.env.JWT_KEY) {
+    throw new Error('JWT is not defined!');
+}
+
+if(!process.env.MONGO_URI) {
+    throw new Error('MONGO URI is not defined!');
+}
+
+const startUp = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI!);
+        app.listen(5001);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startUp();
