d5357317be12170ca49e32738546c8ca4345f65e
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-27 16:31:19 +0300

    create section routers

--- sections/src/routes/section-routes.ts
+++ sections/src/routes/section-routes.ts
@@ -7,4 +7,9 @@ import { getSections, newSection } from '../controllers/section-controllers';
 const router = express.Router();
 
 router.use(checkAuth);
-router.get('/',)
+router.get('/', getSections);
+router.post('/new', [
+    body('name').trim().isLength({min: 3})
+], newSection);
+
+export { router as sectionRouter };
