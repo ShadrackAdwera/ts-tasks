be6bedcf647e65848e7ac31c3c3cfb9dfd297453
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-27 13:10:01 +0300

    create the section created publisher

---
+++ auth/src/events/publishers/section-publisher.ts
@@ -0,0 +1,6 @@
+import { Publisher, SectionCreatedEvent, Subjects } from '@adwesh/v2-common';
+
+class SectionPublisher extends Publisher<SectionCreatedEvent> {
+    subject: Subjects.SectionCreated = Subjects.SectionCreated;
+    
+}
