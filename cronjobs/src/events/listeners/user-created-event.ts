459f1daa44404126174199fdd5cca8ae8544169b
ShadrackAdwera <adweshshaddie@gmail.com>
2022-03-07 13:30:55 +0300

    configure user created listener event

--- cronjobs/src/events/listeners/user-created-event.ts
+++ cronjobs/src/events/listeners/user-created-event.ts
@@ -1,1 +1,27 @@
-//listen to user whose role is agent, if created, create a record in users db
+import { Listener, Subjects, UserCreatedEvent } from '@adwesh/v2-common';
+import { Message } from 'node-nats-streaming';
+import { Agent } from '../../models/Models';
+
+export class UserCreatedListener extends Listener<UserCreatedEvent> {
+    subject: Subjects.UserCreated = Subjects.UserCreated;
+    queueGroupName: string = 'cronjobs-service';
+    async onMessage(data: { id: string; email: string; category: string; }, msg: Message) {
+       //find user in db, if not exists, create them
+       let foundUser;
+       try {
+           foundUser = await Agent.findOne({ userId: data.id }).exec();
+       } catch (error) {
+           throw new Error(<string>error);
+       }
+       if(foundUser) {
+           msg.ack();
+       }
+       try {
+           const newAgent = new Agent({ userId: data.id, email: data.email, category: data.category});
+           await newAgent.save();
+           msg.ack();
+       } catch (error) {
+           throw new Error(error as string);
+       }
+    }
+}
